d25dc4ba63df18874d1f52c4679886b7087ccb0c
William McFall <wmcfall2@gmail.com>
2019-04-09 22:00:27 -0400

    fifth commit

--- index.js
+++ index.js
@@ -7,11 +7,61 @@ var word = require("./word.js");
 var axios = require("axios");
 var inquirer = require("inquirer");
 
-// randomly selects a movie title from OMDB database
+// Creates an array of Nichols Cage movie titles
+var WordArray = [];
+function wordSelect() {
+    axios
+        .get("http://api.tvmaze.com/search/people?q=Nicholas Cage")
+        .then(function (response) {
+            for (i = 0; i < response.data.length; i++) {
+                WordArray = Wordarray + response.data[i].name;
+                console.log(response.data[i].name);
+            }
+        });
+};
+
+wordSelect();
+
+var misses = 0;
+
+if (misses < 10) {
+// draws random word
+
+// presents random word
+
+// asks user to guess letter
+inquirer.prompt([
+    {
+      name: "guessedLetter",
+      message: "Please guess a letter: ",
+      type: "input"
+    }
+]).then(function(answers) {
+    // checks whether guessed letter in random word
+    var letter = new Letter(answers.guessedLetter);
+  });
+
+// addes to missed counter if wrong
+
+}
+else {
+console.log("You lost!");
+inquirer.prompt([
+    {
+      name: "playAgain",
+      message: "Would you like to play again: ",
+      type: "choice"
+    }
+]).then(function(answers) {
+    if(answers.playAgain == "Yes"){
+    
+    }
+    else if (answers.playAgain == "No"){
+    console.log("Ok. Goodbye!");   
+    };
+  });
+};
 
 
-// randomly selects a movie title from OMDB database
 
 
-// runs selected guess word through the Word constructor
-var guessword = new Word("John");
